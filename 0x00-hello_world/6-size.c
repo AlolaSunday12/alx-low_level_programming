7e242864892673fc9582d4d4e01939f0c0cffb99
AlolaSunday12 <alolasj@gmail.com>
2023-02-16 07:42:22 -0800

    intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
+ * main - A program that prints the size of various types on the computer it is compiled and run on
  * Return 0 (success)
  */
 int main(void)
 {
+prinf("size of various types on the computer it is compiled and run on")
 	char a;
 	int b;
 	long int c;
