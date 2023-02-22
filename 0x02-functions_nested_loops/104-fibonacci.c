a391663db9897504f391cc416e26510cca178963
AlolaSunday12 <alolasj@gmail.com>
2023-02-22 06:46:53 -0800

    104 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 
 {
 	int count;
-	unsigned long fib1 = 0, fib2 = 1 sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
 
@@ -28,7 +28,7 @@ int main(void)
 	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_hal2 + fib2_half2;
+		half2 = fib1_half2 + fib2_half2;
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
